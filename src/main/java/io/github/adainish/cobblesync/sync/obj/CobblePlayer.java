f024e82111042e445896de850325782067c72d25
Adenydd Stuut || Winglet <wingletdev@gmail.com>
2024-07-28 16:59:38 +0200

    hopefully fixed an issue with a cheeky solution by force updating the party

--- src/main/java/io/github/adainish/cobblesync/sync/obj/CobblePlayer.java
+++ src/main/java/io/github/adainish/cobblesync/sync/obj/CobblePlayer.java
@@ -2,11 +2,14 @@ package io.github.adainish.cobblesync.sync.obj;
 
 import com.cobblemon.mod.common.Cobblemon;
 import com.cobblemon.mod.common.api.storage.NoPokemonStoreException;
+import com.cobblemon.mod.common.api.storage.party.PartyStore;
+import com.cobblemon.mod.common.api.storage.party.PlayerPartyStore;
 import com.cobblemon.mod.common.api.storage.pc.PCStore;
 import com.cobblemon.mod.common.api.storage.player.PlayerData;
 import com.cobblemon.mod.common.net.messages.client.storage.party.InitializePartyPacket;
 import com.cobblemon.mod.common.net.messages.client.storage.party.SetPartyReferencePacket;
 import com.cobblemon.mod.common.net.messages.client.storage.pc.InitializePCPacket;
+import com.cobblemon.mod.common.pokemon.Pokemon;
 import com.google.gson.JsonObject;
 import io.github.adainish.cobblesync.CobbleSync;
 import io.github.adainish.cobblesync.logging.Logger;
@@ -14,8 +17,10 @@ import io.github.adainish.cobblesync.sync.obj.abstracted.AbstractPlayer;
 import io.github.adainish.cobblesync.sync.obj.interfaces.Identifiable;
 import net.minecraft.server.level.ServerPlayer;
 
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static io.github.adainish.cobblesync.CobbleSync.executor;
 
@@ -164,6 +169,8 @@ public class CobblePlayer extends AbstractPlayer implements Identifiable {
                 InitializePCPacket packet = new InitializePCPacket(this.uuid, Cobblemon.INSTANCE.getStorage().getPC(this.uuid).getBoxes().size(),true);
                 packet.sendToPlayer(player);
                 Cobblemon.INSTANCE.getStorage().getPC(this.uuid).sendTo(player);
+
+
             }
         } catch (NoPokemonStoreException e) {
             Logger.log(e);
@@ -200,16 +207,54 @@ public class CobblePlayer extends AbstractPlayer implements Identifiable {
                 this.partyStore = Cobblemon.INSTANCE.getStorage().getParty(this.uuid).saveToJSON(new JsonObject());
             else {
                 Cobblemon.INSTANCE.getStorage().getParty(this.uuid).clearParty();
-                Cobblemon.INSTANCE.getStorage().getParty(this.uuid).loadFromJSON(this.partyStore);
+                PartyStore pps = Cobblemon.INSTANCE.getStorage().getParty(this.uuid).loadFromJSON(this.partyStore);
                 InitializePartyPacket packet = new InitializePartyPacket(true, this.uuid, 6);
                 packet.sendToPlayer(player);
                 SetPartyReferencePacket referencePacket = new SetPartyReferencePacket(this.uuid);
                 referencePacket.sendToPlayer(player);
                 Cobblemon.INSTANCE.getStorage().getParty(this.uuid).sendTo(player);
+
+                // Create a map where the keys are the original positions and the values are the Pokemon
+                Map<Integer, Pokemon> originalPartyAndIndex = IntStream.range(0, 6)
+                        .filter(i -> pps.get(i) != null)
+                        .boxed()
+                        .collect(Collectors.toMap(i -> i, pps::get));
+
+                // Iterate over the map and set each Pokemon to its original position in the party
+                originalPartyAndIndex.forEach(pps::set);
             }
         } catch (NoPokemonStoreException e) {
             Logger.log(e);
         }
+    }
+
+    public void shuffle(PlayerPartyStore pps) {
+                HashMap<Integer, Pokemon> originalPartyAndIndex = new HashMap<>();
+                HashMap<Integer, Pokemon> newPartyAndIndex = new HashMap<>();
+                List<Integer> indexes = new ArrayList<>();
+                IntStream.range(0, 6).filter(i1 -> pps.get(i1) != null).forEachOrdered(i1 -> {
+                    Integer integer = i1;
+                    indexes.add(integer);
+                    originalPartyAndIndex.put(i1, pps.get(i1));
+                });
+                //load indexes of the positions of the existing pokemon in the party and shuffle them
+                //int between the available indexes in the party
+                IntStream.range(0, 6).forEachOrdered(i -> {
+                    int randomIndex = indexes.get(new Random().nextInt(indexes.size()));
+                    if (pps.get(i) != null && pps.get(randomIndex) != null) {
+                        pps.swap(i, randomIndex);
+                        newPartyAndIndex.put(i, pps.get(i));
+                    }
+                });
+
+                // reassign to original indexes by swapping the pokemons position in the party to their original positions based on the new indexes and the originals
+                originalPartyAndIndex.forEach((i, p) -> {
+                    newPartyAndIndex.forEach((i1, p1) -> {
+                        if (p.equals(p1)) {
+                            pps.swap(i, i1);
+                        }
+                    });
+                });
     }
 
     public void syncPartyLogout() {
